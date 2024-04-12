437318c9c78880f859e9de222c39d961f60be93e
FelFiress <159952960+FelFiress@users.noreply.github.com>
2024-04-11 22:15:23 -0400

    Add files via upload

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log("Hello World");
+
+        Debug.Log("Changes are being registered");
+    }
+}
